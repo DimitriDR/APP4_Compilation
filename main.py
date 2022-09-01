70e877187febbf8deaae1ce6b1da03d304a3d706
DimitriDR <dimitridroeck@gmail.com>
2022-09-01 22:11:37 +0200

    Semblant d'un truc qui marche :|

--- main.py
+++ main.py
@@ -1,54 +1,115 @@
 import sys
 
 current = {}
-previous = {}
+previous_token = {}
+# Défini la position du curseur dans la chaîne de caractères
 position = 0
+ligne = 0
+special_characters = ["/", "(", ")", "{", "}", ";", "="]
+keywords = ["int", "return"]
 
 
-def next_in_string():
-    global position
-
-    text = "1234567 int main() {'" \
-           "    a = 3;" \
-           "    return 0" \
-           "}"
+def next_in_string(text: str):
+    global position, ligne, previous_token, current
 
     text_length = len(text)
 
-    # Boucle parcourant toute la chaîne récupérée depuis le fichier
+    # Boucle parcourant le texte passé en paramètre pour évaluer chacun des caractères de celui-ci
     while position < text_length:
+        read_char = text[position]
+        buffer = None
 
-        if text[position].isnumeric():
-            buffer = text[position]
+        # Si on rencontre un espace, on l'ignore, et on incrémente notre position
+        if read_char.isspace():
+            position = position + 1
+            continue
+
+        # Si le caractère à la position p n'est ni un caractère alphanumérique, ni un caractère autorisé,
+        # alors le caractère n'est pas valide et une erreur est retournée.
+        if not read_char.isalpha() and not read_char.isnumeric() and not (read_char in special_characters):
+            exit(200)
+
+        # Si le caractère est bien un caractère alphanumérique,
+        # on va continuer de parcourir la chaîne pour en dégager un token
+        elif read_char.isalpha():
+            # Initialisation du buffer par le premier caractère valide que l'on trouve
+            buffer = read_char
+            # On augmente la position pour poursuivre sur la chaîne
             position = position + 1
 
-            while text[position] != text_length and text[position].isnumeric():
-                buffer = buffer + "" + text[position]
-                print(buffer)
+            while position < text_length:
+                read_char = text[position]
+                if read_char.isalpha() or read_char.isnumeric():
+                    buffer = buffer + read_char
+                    position = position + 1
+                else:
+                    break
 
-        # print(text[i])
+            # Si current est un dictionnaire vide, inutile de le sauvegarder dans previous
+            if current != {}:
+                previous_token = current
 
-    # return 1
+            current["Type"] = "IDENT"
+            current["Valeur"] = buffer
+
+            return
+
+        elif read_char.isnumeric():
+            buffer = read_char
+            # On augmente la position pour poursuivre sur la chaîne
+            position = position + 1
+
+            while position < text_length:
+                read_char = text[position]
+                if read_char.isnumeric():
+                    buffer = buffer + read_char
+                    position = position + 1
+                else:
+                    break
+
+            if current != {}:
+                previous_token = current
+
+            current["Type"] = "CONST"
+            current["Valeur"] = buffer
+
+            return
+
+        elif read_char in special_characters:
+            position = position + 1
+            return read_char
 
 
 # Token à implémenter prioritairement EOS / CONST / IDENT / ( / ) / { / } / ; / return / int
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Pas d'argument passé en ligne de commande")
-        exit(1)
+        exit(100)
 
     file = sys.argv[1]
     index = file.rfind('.')
 
     if index == -1 or (index != len(file) - 2 and file[index + 1] != 'c'):
         print("Erreur d'extension de fichier")
-        exit(200)
+        exit(101)
 
-    file_descriptor = open(file, 'r')
+    file_descriptor = open(file, "r")
     txt = file_descriptor.read()
-    print(txt)
 
-    # next_in_string()
-    # while next_in_string() != StopIteration:
-    # print("Previous" + previous)
-    # print("Current" + current)
+    print(next_in_string(txt))
+    print(current)
+    print(next_in_string(txt))
+    print(current)
+    print(next_in_string(txt))
+    print(next_in_string(txt))
+    print(next_in_string(txt))
+    print(next_in_string(txt))
+    print(next_in_string(txt))
+    print(current)
+    print(next_in_string(txt))
+    print(next_in_string(txt))
+    print(current)
+    print(next_in_string(txt))
+    print(next_in_string(txt))
+
+    print(next_in_string(txt))
