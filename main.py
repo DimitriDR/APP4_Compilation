022b9cca23cef4cede80ef8cb224ac040be99755
DimitriDR <dimitridroeck@gmail.com>
2022-09-03 12:39:40 +0200

    Petites améliorations, ajout des autres fonctions, nettoyage

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ current = {}
 last = {}
 # Défini la position du curseur dans la chaîne de caractères
 position = 0
+# Variable gardant la ligne en mémoire pour savoir à quelle ligne se situe le token
 ligne = 1
-special_characters = {"/": "tok_slash", "(": "tok_parouvr", ")": "tok_ferm", "{": "tok_accouvr", "}": "tok-accferm",
-                      ";": "tok_ptvirg", "=": "tok_affect", "+": "tok_plus"}
+special_characters = {"/": "tok_slash", "(": "tok_parouvr", ")": "tok_ferm", "{": "tok_accouvr", "}": "tok_accferm",
+                      ";": "tok_ptvirg", "=": "tok_affect", "+": "tok_plus", "*": "tok_star"}
 keywords = ["int", "return", "if", "for", "while", "do", "break", "continue", "else"]
 variables = {}
 
+# Partie analyseur lexical
 
-def next_in_string(text: str):
+"""
+Fonction permettant pour un texte donné, de le parcourir et d'en extraire les tokens
+"""
+
+
+def extract_tokens(text: str):
     global ligne, position, special_characters, keywords, variables
     text_length = len(text)
     tab_token = []
@@ -19,14 +26,14 @@ def next_in_string(text: str):
     # Boucle parcourant le texte passé en paramètre pour évaluer chacun des caractères de celui-ci
     while position < text_length:
         read_char = text[position]
-        buffer = None
 
-        # Si on rencontre un espace, on l'ignore, et on incrémente notre position
+        # Si on trouve un retour à la ligne, on incrémente la variable correspond et on continue de déplacer le curseur
         if read_char == "\n":
             ligne = ligne + 1
             position = position + 1
             continue
 
+        # Si on rencontre un espace, on l'ignore, et on incrémente notre position
         if read_char.isspace():
             position = position + 1
             continue
@@ -34,7 +41,7 @@ def next_in_string(text: str):
         # Si le caractère à la position p n'est ni un caractère alphanumérique, ni un caractère autorisé,
         # alors le caractère n'est pas valide et une erreur est retournée.
         if not read_char.isalpha() and not read_char.isnumeric() and not (read_char in special_characters):
-            exit("caractère non reconnu à la ligne " + str(ligne))
+            exit("Caractère non reconnu à la ligne " + str(ligne))
 
         # Si le caractère est bien un caractère alphanumérique,
         # on va continuer de parcourir la chaîne pour en dégager un token
@@ -44,27 +51,22 @@ def next_in_string(text: str):
             # On augmente la position pour poursuivre sur la chaîne
             position = position + 1
 
+            # On continue d'évoluer dans le texte pour récupérer le prochain caractère
+            # Le prochain caractère peut être une lettre ou un chiffre.
             while position < text_length:
                 read_char = text[position]
-                if read_char.isalpha():
+
+                if read_char.isalpha() or read_char.isnumeric():
                     buffer = buffer + read_char
                     position = position + 1
                 else:
                     break
 
-            # Si current est un dictionnaire vide, inutile de le sauvegarder dans previous
-            # if current != {}:
-            #     previous_token = current
-
-            # current["Type"] = "IDENT"
-            # current["Valeur"] = buffer
-
-            # return
-            if (buffer in keywords):
+            if buffer in keywords:
                 tab_token.append({"type": "tok_" + buffer, "valeur": buffer, "ligne": ligne})
             else:
                 id_var = variables.get(buffer)
-                if (id_var == None):
+                if id_var is None:
                     id_var = len(variables)
                     variables[buffer] = len(variables)
                 tab_token.append({"type": "IDENT", "valeur": id_var, "ligne": ligne})
@@ -99,6 +101,11 @@ def next_in_string(text: str):
     return tab_token
 
 
+"""
+Fonction permettant d'obtenir le token suivant
+"""
+
+
 def next():
     global position, list_tok, current, last
     last = current
@@ -106,7 +113,19 @@ def next():
     position = position + 1
 
 
-# Token à implémenter prioritairement EOS / CONST / IDENT / ( / ) / { / } / ; / return / int
+def check(token_type) -> bool:
+    if current["type"] is token_type:
+        next()
+        return True
+    else:
+        return False
+
+
+def accept(token_type):
+    if not check(token_type):
+        raise Exception("Le type ne correspond pas")
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         exit("Pas d'argument passé en ligne de commande")
@@ -121,28 +140,5 @@ if __name__ == '__main__':
     txt = file_descriptor.readlines()
     txt = "".join(txt)
 
-    list_tok = next_in_string(txt)
+    list_tok = extract_tokens(txt)
     print(list_tok)
-
-    # for i in range(0,10):
-    #     next()
-    #     print(current)
-    #     print(last)
-
-    # print(next_in_string(txt))
-    # print(current)
-    # print(next_in_string(txt))
-    # print(current)
-    # print(next_in_string(txt))
-    # print(next_in_string(txt))
-    # print(next_in_string(txt))
-    # print(next_in_string(txt))
-    # print(next_in_string(txt))
-    # print(current)
-    # print(next_in_string(txt))
-    # print(next_in_string(txt))
-    # print(current)
-    # print(next_in_string(txt))
-    # print(next_in_string(txt))
-
-    # print(next_in_string(txt))
