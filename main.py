2d333a16cf3d93998898e101f58423b1ded75d25
DimitriDR <dimitridroeck@gmail.com>
2022-09-08 12:02:14 +0200

    Début de support des doubles symboles

--- main.py
+++ main.py
@@ -111,7 +111,10 @@ def extract_tokens(text: str):
             tab_token.append({"type": "const", "valeur": buffer, "ligne": ligne})
             continue
 
-        elif read_char in special_characters:  # reste  à traiter le cas des caractere speciaux comportant 2 carcatères
+        elif read_char in special_characters:
+            # TODO: Ajouter support pour && et ||
+            #
+
             # On ajoute le caractère actuel dans un buffer qui servira ... ou pas
             special_characters_buffer = read_char
 
@@ -127,6 +130,11 @@ def extract_tokens(text: str):
                 else:
                     tab_token.append({"type": special_characters[read_char], "valeur": read_char, "ligne": ligne})
                     position += 1
+            else:
+                # Si la position suivante atteint la fin de la chaîne alors,
+                # on enregistre le dernier caractère, incrémente la position et laisse dérouler l'algorithme.
+                tab_token.append({"type": special_characters[read_char], "valeur": read_char, "ligne": ligne})
+                position += 1
 
     tab_token.append({"type": "EOS", "valeur": "EOS", "ligne": ligne})
     position = 0
